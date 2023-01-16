d55b77660897a983d02836b9fcb2810e2007c57a
Hassan HADDANE <hassanhaddane33s@gmail.com>
2023-01-16 16:54:08 +0100

    TD3 - add habitation_details.dart

--- lib/views/habitation_details.dart
+++ lib/views/habitation_details.dart
@@ -1,7 +1,18 @@
-import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
+import 'package:location/models/habitation.dart';
+import 'package:location/share/location_style.dart';
+import 'package:location/share/location_text_style.dart';
+import 'package:tdlocation/models/habitation.dart';
+import 'package:tdlocation/share/location_style.dart';
+import 'package:tdlocation/share/location_text_style.dart';
+import 'package:tdlocation/views/habitation_feature_widget.dart';
+import 'package:tdlocation/views/habitation_option.dart';
 
 class HabitationDetails extends StatefulWidget {
-  const HabitationDetails({Key? key}) : super(key: key);
+  final Habitation _habitation;
+
+  const HabitationDetails(this._habitation, {Key? key}) : super(key: key);
 
   @override
   State<HabitationDetails> createState() => _HabitationDetailsState();
@@ -10,6 +21,87 @@ class HabitationDetails extends StatefulWidget {
 class _HabitationDetailsState extends State<HabitationDetails> {
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(widget._habitation.libelle),
+      ),
+      body: ListView(
+        padding: const EdgeInsets.all(4.0),
+        children: [
+          HabitationFeatureWidget(widget._habitation),
+          _buildItems(),
+          _buildOptionsPayantes(),
+          _buildRentButton()
+        ],
+      ),
+    );
+  }
+
+  _buildRentButton() {
+    var format = NumberFormat('### €');
+
+    return Container(
+      decoration: BoxDecoration(
+          color: LocationStyle.backgroundColorPurple,
+          borderRadius: BorderRadius.circular(8.0)),
+      margin: const EdgeInsets.all(8.0),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: [
+          Container(
+              margin: const EdgeInsets.symmetric(horizontal: 8.0),
+              child: Text(
+                format.format(widget._habitation.prixmois),
+                // TODO : Style
+              )),
+          Container(
+              margin: const EdgeInsets.symmetric(horizontal: 8.0),
+              child: ElevatedButton(
+                  onPressed: () {
+                    print('Louer habitation.');
+                  },
+                  child: const Text('Louer')))
+        ],
+      ),
+    );
+  }
+
+  _buildItems() {
+    return Wrap(
+        spacing: 2.0,
+        children: Iterable.generate(
+            widget._habitation.options.length,
+            (i) => Container(
+                  padding: const EdgeInsets.only(left: 15.0),
+                  margin: const EdgeInsets.all(2.0),
+                  child: Column(
+                    children: [
+                      Text(widget._habitation.options[i].libelle,
+                          style: LocationTextStyle.boldTextStyle),
+                      Text(widget._habitation.options[i].description,
+                          style: LocationTextStyle.regularGreyTextStyle),
+                    ],
+                  ),
+                )).toList());
+  }
+
+  _buildOptionsPayantes() {
+    return Wrap(
+        spacing: 2.0,
+        children: Iterable.generate(
+            widget._habitation.optionsPayantes.length,
+            (i) => Container(
+                  padding: const EdgeInsets.only(left: 15.0),
+                  margin: const EdgeInsets.all(2.0),
+                  child: Column(
+                    children: [
+                      Text(widget._habitation.optionsPayantes[i].libelle,
+                          style: LocationTextStyle.boldTextStyle),
+                      Text(
+                          '${widget._habitation.optionsPayantes[i].prix.toString()} €',
+                          style: LocationTextStyle.regularGreyTextStyle),
+                    ],
+                  ),
+                )).toList());
   }
 }
