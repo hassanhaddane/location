3a83a0316eed66d426a8a68d7ac4084d4df6a17e
Hassan HADDANE <hassanhaddane33s@gmail.com>
2023-01-16 16:34:50 +0100

    TD2 - Update

--- lib/views/habitation_list.dart
+++ lib/views/habitation_list.dart
@@ -1,94 +1,79 @@
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
 import 'package:location/models/habitation.dart';
+import 'package:tdlocation/models/habitation.dart';
+import 'package:tdlocation/views/habitation_feature_widget.dart';
+
+import 'habitation_details.dart';
+import 'habitation_service.dart';
 
 class HabitationList extends StatelessWidget {
   final bool isHouseList;
+  final HabitationService habitationService = HabitationService();
+  late List<Habitation> _habitations;
 
-  HabitationList(this.isHouseList, {Key? key}) : super(key: key);
-
-  final _habitations = [
-    Habitation(2, "appartement.png", "Appartement 2", "Rue 2", 3, 1, 50, 555),
-    Habitation(3, "appartement.png", "Appartement 3", "Rue 3", 2, 1, 51, 401),
-    Habitation(4, "appartement.png", "Appartement 4", "Rue 4", 2, 1, 52, 402),
-  ];
+  HabitationList({Key? key, required this.isHouseList}) : super(key: key) {
+    _habitations = isHouseList
+        ? habitationService.getMaisons()
+        : habitationService.getAppartements();
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title:
-            Text("Liste des ${this.isHouseList ? 'maisons' : 'appartements'}"),
-      ),
-      body: Center(
-        child: ListView.builder(
-          itemCount: _habitaitons.lenght,
+        appBar: AppBar(
+          title: Text('Liste des ${isHouseList ? 'maisons' : 'appartements'}.'),
+        ),
+        body: Center(
+            child: ListView.builder(
+          itemCount: _habitations.length,
           itemBuilder: (context, index) =>
               _buildRow(_habitations[index], context),
           itemExtent: 285,
-        ),
-      ),
-    );
+        )));
   }
 
   _buildRow(Habitation habitation, BuildContext context) {
     return Container(
-      margin: EdgeINsets.all(4.0),
-      child: Column(
-        children: [
-          Container(
-            height: 150,
-            width: MediaQuery.of(context).size.width,
-            child: ClipRRect(
-              borderRadius: BorderRadius.circular(20.0),
-              child: Image.asset(
-                'assets/images/locations/${habitation.image}',
-                fit: BoxFit.fitWidth,
-              ),
-            ),
-          ),
-          _buildDetails(habitation),
-        ],
-      ),
-    );
+        margin: const EdgeInsets.all(4.0),
+        child: GestureDetector(
+            onTap: () {
+              Navigator.push(
+                context,
+                MaterialPageRoute(
+                    builder: (context) => HabitationDetails(habitation)),
+              );
+            },
+            child: _buildDetails(habitation)));
   }
 
   _buildDetails(Habitation habitation) {
-    // var format = NumberFormat("### $");
-    return Container(
-      child: Column(
-        children: [
-          Row(
-            children: [
-              Expanded(
-                flex: 3,
-                child: ListTile(
-                  title: Text(habitation.libelle),
-                  subtitle: Text(habitation.adresse),
-                ),
+    var format = NumberFormat('### €');
+    return Column(
+      children: [
+        Row(
+          children: [
+            Expanded(
+              flex: 3,
+              child: ListTile(
+                title: Text(habitation.libelle),
+                subtitle: Text(habitation.adresse),
               ),
-              Expanded(
-                flex: 1,
-                child: Text(
-                  format.format(habitation.prixmois),
-                  style: TextStyle(
+            ),
+            Expanded(
+              flex: 1,
+              child: Text(
+                format.format(habitation.prixmois),
+                style: const TextStyle(
                     fontWeight: FontWeight.bold,
                     fontFamily: 'Roboto',
-                    fontSize: 22,
-                  ),
-                ),
-              )
-            ],
-          ),
-          Row(
-            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-            children: [
-              HabitationOption(Icons.group, "${habitation.chambres} personnes"),
-              HabitationOption(Icons.fit_screen, "${habitation.superficie} m²"),
-            ],
-          ),
-        ],
-      ),
+                    fontSize: 22),
+              ),
+            )
+          ],
+        ),
+        HabitationFeatureWidget(habitation)
+      ],
     );
   }
 }
