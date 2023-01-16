fe5f856605670a387c6894e28cd3f2389a09c6c3
Hassan HADDANE <hassanhaddane33s@gmail.com>
2023-01-16 16:57:21 +0100

    TD3 - update location_text_style

--- lib/share/location_text_style.dart
+++ lib/share/location_text_style.dart
@@ -1,19 +1,29 @@
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:tdlocation/share/location_style.dart';
 
 import 'location_style.dart';
 
 class LocationTextStyle {
-  static final baseTextStyle = GoogleFonts.getFont('Raleway')
-                                  .copyWith(color: LocationStyle.colorPurple);
+  static final baseTextStyle =
+      GoogleFonts.getFont('Raleway').copyWith(color: LocationStyle.colorPurple);
 
-  static final regularTextStyle = baseTextStyle.copyWith(
-    fontSize: 13
-  );
-  static final regularWhiteTextStyle = baseTextStyle.copyWith(
-    color: Colors.white70,
-    fontSize: 13
-  );
-  static final boldTextStyle = baseTextStyle.copyWith(fontWeight: FontWeight.bold
-  );
+  static final regularTextStyle = baseTextStyle.copyWith(fontSize: 13.0);
+
+  static final regularWhiteTextStyle =
+      baseTextStyle.copyWith(fontSize: 13.0, color: Colors.white70);
+
+  static final priceTextStyle = baseTextStyle.copyWith(
+      fontSize: 16.0, fontWeight: FontWeight.bold, color: Colors.white70);
+
+  static final priceGreyTextStyle = priceTextStyle.copyWith(color: Colors.grey);
+
+  static final regularGreyTextStyle =
+      baseTextStyle.copyWith(fontSize: 13.0, color: Colors.grey);
+
+  static final boldTextStyle =
+      baseTextStyle.copyWith(fontWeight: FontWeight.bold);
+
+  static final subTitleboldTextStyle =
+      baseTextStyle.copyWith(fontWeight: FontWeight.bold, fontSize: 16.0);
 }
